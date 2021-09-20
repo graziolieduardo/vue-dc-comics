b010cddefe785a5b5128d13e9104284cd410ced5
Eduardo <graziolieduardo@gmail.com>
2021-09-20 17:51:43 +0200

    Header Finito

---
+++ src/components/Header.vue
@@ -0,0 +1,97 @@
+<template>
+    <div>
+        <div class="margin-start"></div>
+        <header>
+            <div class="wrapper">
+                <div class="logo-container">
+                    <img src="@/assets/img/dc-logo.png" alt="">
+                </div>
+                <div class="nav-container">
+                    <ul>
+                        <li v-for="(link, index) in links" :key="index">
+                            <a :href="link.url" :class="(link.active) ? 'active' : null">
+                                {{ link.text }}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    </div>
+</template>
+
+<script>
+import Links from '@/assets/js/nav-list'
+
+export default {
+    name: 'Header',
+    data() {
+        return {
+            links: Links
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/style/var-colors';
+    
+    .margin-start {
+        height: 30px;
+        background-color: $black;
+    }
+
+    header {
+        width: 100%;
+        height: 120px;
+        background-color: #fff;
+
+        .wrapper {
+            width: 70%;
+            height: 100%;
+            margin: 0 auto;
+        }
+
+        .logo-container,
+        .nav-container {
+            float: left;
+        }
+
+        .logo-container {
+            display: flex;
+            align-items: center;
+            width: 40%;
+            height: 100%;
+
+            img {
+                width: 80px;
+            }
+        }
+
+        .nav-container {
+            display: flex;
+            align-items: center;
+            width: 60%;
+            height: 100%;
+            padding: 20px;
+            font-size: 12px;
+
+            li {
+                display: inline-block;
+                margin: 15px;
+                text-transform: uppercase;
+
+                a {
+                    color: $secondary-darkgray;
+
+                    &.active {
+                        color: $primary-blue;
+                    }
+                    &:hover {
+                        color: $primary-blue;
+                    }
+                }
+            }
+        }
+    }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <div class="main-top">
+            <div class="wrapper">
+                <h1>Content goes here</h1>
+            </div>
+        </div>
+        <div class="main-bottom">
+            <div class="wrapper">
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/style/var-colors';
+
+    .main-top {
+        margin: 0 auto;
+        background-color: $black;
+
+        .wrapper {
+            width: 70%;
+            margin: 0 auto;
+        }
+
+        h1 {
+            display: flex;
+            align-items: center;
+            width: 100%;
+            height: 150px;
+            color: #fff;
+        }
+    }
+
+    .main-bottom {
+        width: 100%;
+        height: 150px;
+        background-color: $primary-blue;
+
+        .wrapper {
+            width: 70%;
+            margin: 0 auto;
+        }
+    }
+</style>
